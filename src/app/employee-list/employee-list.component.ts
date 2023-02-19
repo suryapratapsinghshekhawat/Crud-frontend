cbd53c667b61c94570b4dcd09e0d141353353b12
suryapratapsinghshekhawat <suryapratapspss129812@gmail.com>
2023-02-20 01:35:22 +0530

    second commit after connecting frontend to backend

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent {
+
+  employees!: Employee[];
+  constructor(private employeeService : EmployeeService){}
+
+
+ngOnInit():void{
+     
+     this.employeeService.getEmployeeList().subscribe(data=>{
+      this.employees=data;
+     });
+
+
+
+
+
+
+
+  // this.employees=[                                                       // creating dummy data
+  //          {
+  //     "id":1,
+  //    "firstName":"Surya Pratap",
+  //    "lastName":"Singh",
+  //    "emailId":"surya@123"
+  //   },
+  //   {
+  //     "id":2,
+  //    "firstName":"Karan",
+  //    "lastName":"Singh",
+  //    "emailId":"karan@123"
+  //   },
+  // ]
+
+
+
+
+}
 
 }
