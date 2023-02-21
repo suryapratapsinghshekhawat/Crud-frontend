7d14adc2ef95317c6321907607bcb031a0cd08a7
suryapratapsinghshekhawat <suryapratapspss129812@gmail.com>
2023-02-21 07:23:37 +0530

    routing commits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'employee',  component:EmployeeListComponent},
+  {path:'', redirectTo:'employee', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
